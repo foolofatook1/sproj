e9d598654bf81f4637241e6680aa91adf55f00b5
d1x13 <jf9260@bard.edu>
2019-02-10 12:21:14 -0500

    post text screen and sprite appearance

--- kani_kousen/level_1/level_1.c
+++ kani_kousen/level_1/level_1.c
@@ -16,6 +16,9 @@
 #include "assets/text/text_2_level_1.c" /* second text box */
 #include "assets/text/text_3_level_1.c" /* third text box */
 
+/* shit pot assets */
+#include "assets/shit_pot_tiles.c" 
+#include "assets/shit_pot.c"
 
 /* starting sprites */
 #include "../assets/sprites/hero_front_idle.c"
@@ -30,6 +33,7 @@ void output_text(void);
 
 UINT8 text_count = 0;
 UINT8 hero_pos[2][2];
+UINT8 fisherman_pos[2][2];
 
 /* The big control function */
 void level_1_ctrl(void)
@@ -65,13 +69,9 @@ void level_1_bkg_start(void)
     HIDE_WIN;
     SHOW_BKG;
 
-    HIDE_SPRITES;
-
     /* setting up first set of text */
     set_bkg_data(0, 55, font);
     set_bkg_tiles(0, 0, 20, 18, text_1_level_1);
-
-    //level_1_sprite_setup();
 
     DISPLAY_ON;
 }
@@ -101,25 +101,36 @@ void output_text(void)
         set_bkg_tiles(0,0,20,18,text_3_level_1);
         DISPLAY_ON;
     }
-    /*
-    if(text_count == 3)
-    {}*/
+    if(text_count >= 3)
+    {
+        DISPLAY_OFF;
+        scroll_bkg(40,0); /* center on the door */
+        set_bkg_data(0, 16, shit_pot_tiles);
+        set_bkg_tiles(0,0,32,32,shit_pot);
+        DISPLAY_ON;
+        HIDE_SPRITES;
+        level_1_sprite_setup();
+    }
 }
 
 void level_1_sprite_setup(void)
 {
+
     UINT8 sprite_width = 8;
+   
+    /* hero on screen at door */
     /* left half */
-    hero_pos[0][0] = 15;
-    hero_pos[0][1] = 90;
+    hero_pos[0][0] = 70; 
+    hero_pos[0][1] = 65;
     /* right half */
-    hero_pos[1][0] = hero_pos[0][0]+8;
+    hero_pos[1][0] = hero_pos[0][0]+8; /* 8 is pixel width */
     hero_pos[1][1] = hero_pos[0][1];
 
     SPRITES_8x16;
     set_sprite_data(0, 7, hero_front_idle);
     set_sprite_tile(0, 0);
     set_sprite_tile(1, 2);
+
     move_sprite(0, hero_pos[0][0], hero_pos[0][1]);
     move_sprite(1, hero_pos[1][0], hero_pos[1][1]);
 
