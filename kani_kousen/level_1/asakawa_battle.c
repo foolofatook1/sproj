7cf541048cbf42cd1226bc8b748b267a3ea21151
d1x13 <jf9260@bard.edu>
2019-02-25 09:43:34 -0500

    starting the battle scene as a seperate c file right now. will implement mem banking later

--- kani_kousen/level_1/asakawa_battle.c
+++ kani_kousen/level_1/asakawa_battle.c
@@ -1,19 +1,64 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
-//#include "../text/text.c"
-//#include "assets/small_chain_border.c"
+/* sprites */
+#include "../assets/sprites/hero_idle_back.c"
+#include "../assets/sprites/asakawa_front_idle.c"
 
-void asakawa_battle1_setup(void)
+/* bkg */
+#include "../text/text.c"
+#include "assets/small_chain_border.c"
+
+UINT8 hero_pos[2][2];
+UINT8 asakawa_pos[2][2];
+
+UINT8 SPRITE_WIDTH = 8;
+
+void main(void)
 {
+    wait_vbl_done();
+
     DISPLAY_OFF;
     HIDE_BKG;
     HIDE_SPRITES;
-   
- //   set_win_data(0,10,chain_border_tiles);
-  //  set_win_tiles(0,8,20,18,small_chain_border);
-    
-    SHOW_WIN;
-    DISPLAY_ON;
+    HIDE_WIN;
 
+    SPRITES_8x16;
+
+    /* setup the bkg */
+    set_bkg_data(0,10, chain_border_tiles);
+    set_bkg_tiles(0,0,20,18,small_chain_border);
+
+    /* setup sprites */
+    /* hero */
+    hero_pos[0][0] = 80;
+    hero_pos[0][1] = 72;
+    hero_pos[1][0] = hero_pos[0][0]+SPRITE_WIDTH;
+    hero_pos[1][1] = hero_pos[0][1];
+
+    set_sprite_data(0, 8, hero_idle_back);
+    set_sprite_tile(0, 0);
+    set_sprite_tile(1, 2);
+    /* asakawa */
+    asakawa_pos[0][0] = 80;
+    asakawa_pos[0][1] = 40;
+    asakawa_pos[1][0] = asakawa_pos[0][0]+SPRITE_WIDTH;
+    asakawa_pos[1][1] = asakawa_pos[0][1];
+
+    set_sprite_data(8, 8, asakawa_front_idle);
+    set_sprite_tile(2, 8);
+    set_sprite_tile(3, 10);
+
+
+    /* display sprites */
+    /* hero */
+    move_sprite(0, hero_pos[0][0], hero_pos[0][1]);
+    move_sprite(1, hero_pos[1][0], hero_pos[1][1]);
+    /* asakawa */
+    move_sprite(2, asakawa_pos[0][0], asakawa_pos[0][1]);
+    move_sprite(3, asakawa_pos[1][0], asakawa_pos[1][1]);
+
+    SHOW_BKG;
+    SHOW_SPRITES;
+    DISPLAY_ON;
 }
