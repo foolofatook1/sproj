617e472b19e934b23d999294cf08b9d163b88217
d1x13 <jf9260@bard.edu>
2019-02-27 11:48:24 -0500

    added more to the battle navigation

--- kani_kousen/level_1/asakawa_battle.c
+++ kani_kousen/level_1/asakawa_battle.c
@@ -1,26 +1,4 @@
-#include <gb/gb.h>
-#include <stdio.h> 
-/* sprites */
-#include "../assets/sprites/hero_idle_back.c"
-#include "../assets/sprites/asakawa_front_idle.c"
-
-/* bkg */
-#include "../text/text.c"
-
-
-void asakawa_battle_ctrl(void);
-void battle_menu(void);
-void asakawa_battle_check(void);
-void show_sprites(void);
-
-
-UINT8 hero_pos[2][2];
-UINT8 asakawa_pos[2][2];
-
-UINT8 arrow_x = 16;
-UINT8 arrow_y = 92;
-
-UINT8 SPRITE_WIDTH = 8;
+#include "asakawa_battle.h"
 
 void asakawa_battle_ctrl(void)
 {
@@ -30,6 +8,34 @@ void asakawa_battle_ctrl(void)
         asakawa_battle_check();
         //show_sprites();
     }
+}
+
+void battle_nav(void)
+{
+    if(arrow_y == 32)
+    {
+        fight_opt();
+        state = FIGHT_OPT;
+    }
+}
+
+void back(void)
+{
+    if(state < FIGHTING)
+    {
+        battle_menu();
+        state = BATTLE_OPT;
+    }
+}
+
+void fight_opt(void)
+{
+    sprite_clean();
+    battle_bkg_clean();
+    battle_print(">", 18, 32);
+    battle_print(" punch", 18, 32);
+    show_fighter();
+    LETTER_COUNT = 0;
 }
 
 void battle_menu(void)
@@ -42,67 +48,93 @@ void battle_menu(void)
     hide_sprites();
     /* selection menu */
     battle_print(">", arrow_x, arrow_y);
-    battle_print(" fight", 16, 92);
-    battle_print(" defend", 16, 108);
-    battle_print(" run", 16, 124);
-    battle_print(" item", 16, 140);
+    battle_print(" fight", 18, 32);
+    battle_print(" defend", 18, 48);
+    battle_print(" run", 18, 64);
+    battle_print(" item", 18, 80);
+    show_fighter();
+}
+
+void show_fighter(void)
+{
     /* HP */
-    battle_print("hero", 88, 100);
-    battle_print("hp", 88, 116);
-    battle_print("10/10", 120, 116);
+    battle_print("hero", 88, 40);
+    battle_print("hp", 88, 56);
+    battle_print("10/10", 120, 56);
     LETTER_COUNT = 0;
+}
+
+void battle_toggle_up(void)
+{
+    /* if at top && up */
+    if(arrow_y == 32)
+    {
+        arrow_y = 80;
+        battle_bkg_clean();
+        delay(100);
+        battle_bkg_clean();
+        battle_print(">", arrow_x, arrow_y);
+        LETTER_COUNT = 0;
+        delay(100);
+    }
+    else
+    {
+        battle_bkg_clean();
+        delay(100);
+        battle_bkg_clean();
+        battle_print(">", arrow_x, arrow_y-=16);
+        LETTER_COUNT = 0;
+        delay(100);
+    }
+}
+
+void battle_toggle_down(void)
+{
+    /* if at bottom && down */
+    if(arrow_y == 80)
+    {
+        arrow_y = 32;
+        battle_bkg_clean();
+        delay(100);
+        battle_bkg_clean();
+        battle_print(">", arrow_x, arrow_y);
+        LETTER_COUNT = 0;
+        delay(100);
+    }
+    else
+    {
+        battle_bkg_clean();
+        delay(100);
+        battle_bkg_clean();
+        battle_print(">", arrow_x, arrow_y+=16);
+        LETTER_COUNT = 0;
+        delay(100);
+    }
 }
 
 void asakawa_battle_check(void)
 {
     SPRITES_8x8;
-    //if(joypad() & J_A);
-    if(joypad() & J_DOWN)
-    {
-        /* check the arrow */
-        if(arrow_y == 140)
-        {
-            arrow_y = 92;
-            battle_bkg_clean();
-            delay(100);
-            battle_bkg_clean();
-            battle_print(">", arrow_x, arrow_y);
-            LETTER_COUNT = 0;
-            delay(100);
-        }
-        else
-        {
-            battle_bkg_clean();
-            delay(100);
-            battle_bkg_clean();
-            battle_print(">", arrow_x, arrow_y+=16);
-            LETTER_COUNT = 0;
-            delay(100);
-        }
-    }
-    if(joypad() & J_UP)
-    {
-        /* check the arrow */
-        if(arrow_y == 92)
-        {
-            arrow_y = 140;
-            battle_bkg_clean();
-            delay(100);
-            battle_bkg_clean();
-            battle_print(">", arrow_x, arrow_y);
-            LETTER_COUNT = 0;
-            delay(100);
-        }
-        else
-        {
-            battle_bkg_clean();
-            delay(100);
-            battle_bkg_clean();
-            battle_print(">", arrow_x, arrow_y-=16);
-            LETTER_COUNT = 0;
-            delay(100);
-        }
-    }
+
+    if(joypad() & J_A)
+        battle_nav();
+
+    if(joypad() & J_B)
+        back();
+
+    /* toggle down options */
+    if(joypad() & J_DOWN && state == BATTLE_OPT)
+        battle_toggle_down();
+    /*if(joypad() & J_DOWN && state == FIGHT_OPT)
+    if(joypad() & J_DOWN && state == ITEM_OPT)*/
+
+    /* toggle up options */
+    if(joypad() & J_UP && state == BATTLE_OPT)
+        battle_toggle_up();
+/*    if(joypad() & J_DOWN && state == FIGHT_OPT)
+    if(joypad() & J_DOWN && state == ITEM_OPT)*/
+
+
 }
 
 void show_sprites(void)
@@ -147,7 +179,7 @@ void main(void)
     HIDE_BKG;
     HIDE_SPRITES;
     HIDE_WIN;
- 
+
     battle_menu();
 
     SHOW_BKG;
---
+++ kani_kousen/level_1/asakawa_battle.h
@@ -0,0 +1,40 @@
+#include <gb/gb.h>
+#include <stdio.h> 
+/* sprites */
+#include "../assets/sprites/hero_idle_back.c"
+#include "../assets/sprites/asakawa_front_idle.c"
+
+/* bkg */
+#include "../text/text.c"
+
+
+void asakawa_battle_ctrl(void);
+void battle_menu(void);
+void battle_toggle_up(void);
+void battle_doggle_down(void);
+void asakawa_battle_check(void);
+void show_sprites(void);
+void battle_nav(void);
+void fight_opt(void);
+void run(void);
+void show_items(void);
+void show_fighter(void);
+void back(void);
+
+
+#define SPRITE_WIDTH 8
+
+#define BATTLE_OPT  0
+#define FIGHT_OPT   1
+#define RUN         2
+#define ITEM_OPT    3
+#define FIGHTING    4
+
+
+UINT8 hero_pos[2][2];
+UINT8 asakawa_pos[2][2];
+
+UINT8 arrow_x = 18;
+UINT8 arrow_y = 32;
+
+UINT8 state = BATTLE_OPT;
